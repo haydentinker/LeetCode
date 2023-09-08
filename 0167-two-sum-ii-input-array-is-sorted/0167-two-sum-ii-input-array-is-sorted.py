2228b61195f23e708479ebf0705f8c99fa3061cf
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-09-08 16:49:54 -0700

    Time: 121 ms (62.92%), Space: 17.4 MB (14.52%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        resultMap={}
-        for index,i in  enumerate(numbers):
-            if i in resultMap.keys():
-                return [resultMap[i],index+1]
+        # resultMap={}
+        # for index,i in  enumerate(numbers):
+        #     if i in resultMap.keys():
+        #         return [resultMap[i],index+1]
+        #     else:
+        #         resultMap[target-i]=index+1
+        left,right=0,len(numbers)-1
+        while left<right:
+            newSum=numbers[left]+numbers[right]
+            if newSum>target:
+                right-=1
+            elif newSum<target:
+                left+=1
             else:
-                resultMap[target-i]=index+1
+                return [left+1,right+1]
         return []
