8f0d28a1ffc281103ecd901288013d31338c92d3
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-09-09 12:01:37 -0700

    Time: 35 ms (83.47%), Space: 16.4 MB (50.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) <2:
-            return False
         myStack=[]
         for i in s:
             if (i=='(') or (i=='{') or (i=='['):
