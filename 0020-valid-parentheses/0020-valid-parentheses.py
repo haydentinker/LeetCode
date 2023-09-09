862a2f27037cf9c8882dbd22f70b8508e4d90f9e
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-09-09 12:04:43 -0700

    Time: 41 ms (54.8%), Space: 16.3 MB (50.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,12 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         myStack=[]
         for i in s:
+            #If it is an opening add to stack
             if (i=='(') or (i=='{') or (i=='['):
                 myStack.append(i)
             else:
+                #Check to make sure there are items on the stack
                 if not myStack:
                     return False
                 else:
+                    #If there is a closing bracket pop off stack and if it doesn't match
+                    # return false
                     if i ==']':
                         if not myStack.pop()=='[':
                             return False
@@ -17,6 +21,7 @@ class Solution:
                     elif i=='}':
                         if not myStack.pop()=='{':
                             return False 
+        #If there are still elements on the stack after loop return false
         if myStack:
             return False 
-        return True            
+        return True
