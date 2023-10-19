7e654dd2da5d03a23bb8a1e767187fbd5ece5c87
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-10-19 12:13:23 -0700

    Time: 528 ms (62%), Space: 31.1 MB (43.77%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,17 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # First soltion that uses O(n) extra space
-        # Result: 522 ms and 32.2 MB
-        # Faster but uses a little bit more memory
-        # foundNums=set()
-        # for i in nums:
-        #     if i in foundNums:
-        #         return i
-        #     foundNums.add(i)
-        # return 0
-        # Floyd's algorithm
-        # Result: 551ms and 30.9 MB
-        # Slow but uses less memory
+        
         fast,slow=0,0
         while True:
             slow=nums[slow]
