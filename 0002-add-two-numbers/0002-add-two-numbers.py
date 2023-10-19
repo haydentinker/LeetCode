57f341d6a0bcd13f9e50f24396658cea253ae42d
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-10-19 16:20:35 -0700

    Time: 58 ms (87.87%), Space: 16.4 MB (12.8%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,13 +12,9 @@ class Solution:
             value1= l1.val if l1 else 0
             value2= l2.val if l2 else 0
             currentSum=value1+value2+carry
-            carry=0
+            carry=currentSum // 10
+            result.next=ListNode(val=currentSum%10)
     
-            if currentSum>=10:
-                result.next=ListNode(val=currentSum%10)
-                carry=1
-            else:
-                result.next=ListNode(val=currentSum)
             result=result.next
             l1=l1.next if l1 else None
             l2=l2.next if l2 else None
