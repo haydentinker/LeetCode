60efed628cdbc48ca2814e1ecadfaa6523722cc2
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-10-19 16:22:40 -0700

    Time: 69 ms (33.13%), Space: 16.4 MB (43.26%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,21 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #Init result LinkedList, dummy pointer, and carry
         result=ListNode()
         resultPointer=result
         carry=0
         while l1 or l2 or carry:
+            #Grab list vals else 0
             value1= l1.val if l1 else 0
             value2= l2.val if l2 else 0
+            #Add up
             currentSum=value1+value2+carry
+            # Set carry and sum
             carry=currentSum // 10
             result.next=ListNode(val=currentSum%10)
-    
+            # Update pointers to next
             result=result.next
             l1=l1.next if l1 else None
             l2=l2.next if l2 else None
         
-
-
-
         return resultPointer.next
