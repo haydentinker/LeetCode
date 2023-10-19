cb93245c68fefd50efad5c1202dba9163f68ba45
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-10-19 12:11:50 -0700

    Time: 551 ms (33.33%), Space: 30.9 MB (86.14%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,8 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        foundNums=set()
-        for i in nums:
-            if i in foundNums:
-                return i
-            foundNums.add(i)
+        # First soltion that uses O(n) extra space
+        # foundNums=set()
+        # for i in nums:
+        #     if i in foundNums:
+        #         return i
+        #     foundNums.add(i)
+        # return 0
+        # Floyd's algorithm
+        fast,slow=0,0
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+            if fast ==slow:
+                break
+        slow2=0
+        while True:
+            slow2=nums[slow2]
+            slow=nums[slow]
+            if slow==slow2:
+                return slow
         return 0
