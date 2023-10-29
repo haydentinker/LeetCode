11cf968e288700a21c7c490c2b9192d25da38356
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-10-29 10:46:15 -0700

    Time: 43 ms (71.31%), Space: 16.3 MB (93.38%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result=[]
-
-        def dfs(current):
-            if len(current)>=len(nums):
-                if current not in result:
-                    result.append(current)
-                return 
-            for i in nums:
-                if i not in current:
-                    dfs(current+[i])
-
-        dfs([])
+        #Suboptimal solution
+        # def dfs(current):
+        #     if len(current)>=len(nums):
+        #         if current not in result:
+        #             result.append(current)
+        #         return 
+        #     for i in nums:
+        #         if i not in current:
+        #             dfs(current+[i])
+        #optimal solution
+        if (len(nums)==1):
+            return [nums.copy()]
+        for i in range(len(nums)):
+            n=nums.pop(0)
+            perms=self.permute(nums)
+            for perm in perms:
+                perm.append(n)
+            result.extend(perms)
+            nums.append(n)
         return result
+
+        
