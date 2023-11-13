4fe9882c84a9146caa57f13472cdcf81e937d4db
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-11-12 19:07:01 -0800

    Time: 36 ms (83.45%), Space: 16.4 MB (7.23%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,5 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        # One Solution
+        # nums.sort()
+
+        left=0
+        right=len(nums)-1
+
+        while left<=right:
+            print(nums)
+            curNum=nums[left]
+            del nums[left]
+            if curNum==0:
+                left+=1
+                nums.insert(0,0)
+            elif curNum==2:
+                right-=1
+                nums.append(2)
+            else:
+                nums.insert(left,1)
+                left+=1
             
