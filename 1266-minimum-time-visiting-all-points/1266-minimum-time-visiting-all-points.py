29d865f4dac76972ddf317b7e83c14ff121b5876
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-12-02 18:00:21 -0800

    Time: 66 ms (46.88%), Space: 16.4 MB (30.54%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -4,15 +4,16 @@ class Solution:
         for i in range(len(points)-1):
             currPoint=points[i]
             nextPoint=points[i+1]
-            xDifference=abs(nextPoint[0]-currPoint[0])
-            yDifference=abs(nextPoint[1]-currPoint[1])
-            if abs(xDifference-yDifference):
-                while xDifference and yDifference:
-                    result+=1
-                    xDifference-=1
-                    yDifference-=1
-                result+=yDifference+xDifference
-            else:
-                result+=xDifference
+            result+=max(abs(nextPoint[0]-currPoint[0]),abs(nextPoint[1]-currPoint[1]))
+            # xDifference=abs(nextPoint[0]-currPoint[0])
+            # yDifference=abs(nextPoint[1]-currPoint[1])
+            # if abs(xDifference-yDifference):
+            #     while xDifference and yDifference:
+            #         result+=1
+            #         xDifference-=1
+            #         yDifference-=1
+            #     result+=yDifference+xDifference
+            # else:
+            #     result+=xDifference
                 
         return result
