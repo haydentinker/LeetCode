e31ababd6e79fa7ecc540983657e0c84ca1be67d
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-12-02 17:59:13 -0800

    Time: 232 ms (7.44%), Space: 16.3 MB (30.54%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        result=0
+        for i in range(len(points)-1):
+            currPoint=points[i]
+            nextPoint=points[i+1]
+            xDifference=abs(nextPoint[0]-currPoint[0])
+            yDifference=abs(nextPoint[1]-currPoint[1])
+            if abs(xDifference-yDifference):
+                while xDifference and yDifference:
+                    result+=1
+                    xDifference-=1
+                    yDifference-=1
+                result+=yDifference+xDifference
+            else:
+                result+=xDifference
+                
+        return result
