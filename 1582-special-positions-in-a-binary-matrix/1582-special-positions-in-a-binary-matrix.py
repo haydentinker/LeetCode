3297aa3356f693baf2704cfcdfa42fa48d7daa3e
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2023-12-12 17:19:09 -0800

    Time: 149 ms (81.04%), Space: 16.7 MB (64.83%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.py
@@ -6,7 +6,6 @@ class Solution:
             total=sum(i)
             if total==1:
                 rows.append(i.index(1))
-        cols=Counter(rows)
         for idx, col in enumerate(zip(*mat)):
             if idx in rows and sum(col) == 1:
                 result+=1
