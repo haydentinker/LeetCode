ee24c79a8aac44b7a687800e4e7e4973c6822161
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2024-01-05 10:42:02 -0800

    Time: 68 ms (36.34%), Space: 45.8 MB (62.72%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.ts
@@ -0,0 +1,12 @@
+function majorityElement(nums: number[]): number {
+    let count=0;
+    let result=0;
+    for (const num of nums){
+        if(count==0){
+            result=num;
+        }
+        count+=(num==result) ?1:-1;
+
+    }
+    return result;
+};
