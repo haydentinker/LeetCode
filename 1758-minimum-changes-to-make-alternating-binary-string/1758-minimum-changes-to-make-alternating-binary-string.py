6c134e429dc7ec482053d7926d7eeb4686a48123
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2026-03-05 08:54:03 -0500

    Time: 66 ms (5.58%), Space: 24.4 MB (7.57%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,11 +1,20 @@
 class Solution:
+    def __init__(self):
+        self.cache = {}
+
     def minOperations(self, s: str) -> int:
-        oneStartingCount, zeroStartingCount= 0,0
-        for i in range(len(s)):
-            expected1Pattern = '0' if i % 2 == 0 else '1'
-            expected0Pattern = '1' if i % 2 == 0 else '0'
-            if not s[i] == expected1Pattern:
-                oneStartingCount +=1
-            if not s[i] == expected0Pattern:
-                zeroStartingCount +=1
-        return min(oneStartingCount, zeroStartingCount)
+        return min(self.calculate(s, 0, '0'), self.calculate(s, 0, '1'))
+    def calculate(self, s, i, expected):
+            if i == len(s):
+                return 0
+
+            key = (i, expected)
+            if key in self.cache:
+                return self.cache[key]
+
+            flip = 0 if s[i] == expected else 1
+            next_expected = '1' if expected == '0' else '0'
+
+            res = flip + self.calculate(s, i + 1, next_expected)
+            self.cache[key] = res
+            return res
