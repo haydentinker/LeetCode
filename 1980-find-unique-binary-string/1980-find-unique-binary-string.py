f7ae03651b2b6991f84b8f77494d821bbde113f1
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2026-03-08 09:22:55 -0400

    Time: 0 ms (100%), Space: 19.2 MB (99.21%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,13 +1,5 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # valuesAvailable=(2**len(nums[0]))
-        # decimalList=[]
-        # for i in nums:
-        #     decimalList.append(int(i,2))
-        # for i in range(valuesAvailable):
-        #     if i not in decimalList:
-        #         return bin(i)[2:].zfill(len(nums[0]))
-        # Cantor's Diagnoal Arg
         result=[]
         for i in range(len(nums)):
             if nums[i][i]=='1':
@@ -15,4 +7,3 @@ class Solution:
             else:
                 result.append('1')
         return ''.join(result)
-        
