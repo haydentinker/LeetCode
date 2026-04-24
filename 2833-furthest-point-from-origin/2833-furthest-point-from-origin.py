66685174e0d68e7576827d5f66ca6c6547f1d8da
Hayden Tinker <37011292+haydentinker@users.noreply.github.com>
2026-04-24 07:02:14 -0400

    Time: 0 ms (100%), Space: 19.4 MB (18.15%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.py
@@ -2,9 +2,5 @@ from collections import Counter
 class Solution:
     def furthestDistanceFromOrigin(self, moves: str) -> int:
         counts = Counter(moves)
-        res = counts['_']
-        if counts['L'] > counts['R']:
-            res += counts['L'] - counts['R']
-        else:
-            res += counts['R'] - counts['L']
+        res = counts['_'] + abs(counts['L'] - counts['R'])
         return res
