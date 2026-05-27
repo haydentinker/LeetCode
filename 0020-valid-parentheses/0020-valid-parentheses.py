21907a9fea59a9c65039841ff1c386fa3536414f
Hayden Tinker <haydentinker613@gmail.com>
2026-05-27 07:35:25 -0400

    Time: 0 ms (100%), Space: 19.4 MB (6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,27 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        myStack=[]
+        pMap={
+            '{':'}',
+            '(':')',
+            '[':']'
+        }
+    
+        pStack=[]
         for i in s:
-            #If it is an opening add to stack
-            if (i=='(') or (i=='{') or (i=='['):
-                myStack.append(i)
+            if i == '{' or i=='(' or i =='[':
+                pStack.append(i)
             else:
-                #Check to make sure there are items on the stack
-                if not myStack:
-                    return False
+                if len(pStack):
+                    curr = pStack.pop()
+                    if pMap[curr] != i:
+                        return False
                 else:
-                    #If there is a closing bracket pop off stack and if it doesn't match
-                    # return false
-                    if i ==']':
-                        if not myStack.pop()=='[':
-                            return False
-                    elif i ==')':
-                        if not myStack.pop()=='(':
-                            return False 
-                    elif i=='}':
-                        if not myStack.pop()=='{':
-                            return False 
-        #If there are still elements on the stack after loop return false
-        if myStack:
-            return False 
-        return True
+                    return False
+        return True if len(pStack) == 0 else False
