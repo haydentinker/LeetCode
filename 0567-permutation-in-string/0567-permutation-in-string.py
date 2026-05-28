de66e4c34a16190392816d8c8736b925356472c9
Hayden Tinker <haydentinker613@gmail.com>
2026-05-28 10:16:21 -0400

    Time: 12 ms (88.16%), Space: 19.2 MB (86.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,10 +1,21 @@
-from collections import Counter
 class Solution:
-
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        k = len(s1)
-        for i in range(len(s2) - k +1):
-            substring = s2[i:i+k]
-            if Counter(s1) == Counter(substring):
+        s1Len, s2Len = len(s1) , len(s2)
+        if s1Len > s2Len:
+            return False
+        s1Chars, window = [0] *26, [0] *26
+        for i in range(s1Len):
+            s1Chars[ord(s1[i]) - ord('a')] +=1
+            window[ord(s2[i])-ord('a')] +=1
+        
+        if s1Chars == window:
+            return True
+
+        for i in range(s2Len - s1Len):
+            window[ord(s2[i])- ord('a')] -=1
+            window[ord(s2[s1Len + i]) - ord('a')] +=1
+
+            if window == s1Chars:
                 return True
         return False
+
